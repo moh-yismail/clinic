7e5e6f9b394137a2549ec9d55f925708e5f69ee9
moh-yismail <moh.yismail@gmail.com>
2018-10-25 10:03:54 +0300

    setup angular conf

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase, 'Clinic')
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
